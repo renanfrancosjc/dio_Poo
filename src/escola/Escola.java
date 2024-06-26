35e29abf023f9f24c1761e4a54c371f5bfede2b8
renanfrancosjc <98478107+renanfrancosjc@users.noreply.github.com>
2024-06-26 13:39:11 -0300

    src/escola/Escola.javasrc/escola/Aluno.java

---
+++ src/escola/Escola.java
@@ -0,0 +1,12 @@
+package escola;
+
+public class Escola{
+    public static void main(String[] args){
+        Aluno aluno = new Aluno;
+        aluno.setNome("Felipe");
+        aluno.setIdade(8);
+
+        System.out.println("O aluno " + aluno.nome + " tem " + aluno.idade + " anos");
+    }
+
+}
